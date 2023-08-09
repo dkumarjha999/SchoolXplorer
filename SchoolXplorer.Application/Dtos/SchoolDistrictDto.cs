ce0d30f309fe5b24503533c972a7104a7ce22d80
kumardeepak99 <dkumarjha999@gmail.com>
2023-08-09 15:09:00 +0530

    code review fixes.

--- SchoolXplorer.Application/Dtos/SchoolDistrictDto.cs
+++ SchoolXplorer.Application/Dtos/SchoolDistrictDto.cs
@@ -2,12 +2,12 @@
 {
 	public class SchoolDistrictDto
 	{
-		public string Id { get; init; }
-		public string Name { get; init; }
-		public string Description { get; init; }
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
 		public string City { get; set; }
-		public string Superintendent { get; init; }
-		public bool IsPublic { get; init; }
-		public int NumberOfSchools { get; init; }
+		public string Superintendent { get; set; }
+		public bool IsPublic { get; set; }
+		public int NumberOfSchools { get; set; }
 	}
 }
